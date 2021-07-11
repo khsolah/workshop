686bb7dd3a0d1333bb78df32c8c6d9da0a2e5904
James Wu <james_wu@appdet.com>
2021-07-11 20:46:14 +0800

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,72 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <header
+    class="
+      bg-transparent
+      flex
+      bg-opacity-80
+      h-20
+      w-full
+      top-0
+      right-0
+      left-0
+      z-99
+      fixed
+      items-center
+      lg:px-10
+      xl:px-15
+    "
+  >
+    <router-link
+      :to="{ name: 'Index' }"
+      class="
+        font-bold
+        text-white text-base text-3xl
+        no-underline
+        uppercase
+        !bg-transparent
+      "
+      >logo</router-link
+    >
+    <nav class="flex h-full ml-auto mr-0 items-center">
+      <ul class="flex list-none h-10 pl-0 items-center">
+        <li class="h-full text-white ml-5">
+          <router-link
+            :to="{ name: 'Index' }"
+            class="
+              rounded-full
+              flex
+              font-semibold
+              h-full
+              px-6
+              active:bg-white
+              items-center
+              justify-center
+            "
+            >Home</router-link
+          >
+        </li>
+      </ul>
+    </nav>
+  </header>
   <router-view />
+  <footer></footer>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+* {
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  box-sizing: border-box;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  min-height: 100vh;
+  background-image: linear-gradient(#2b1055, #7597de);
 }
 
 #nav {
@@ -26,5 +80,9 @@
       color: #42b983;
     }
   }
+}
+
+.router-link-exact-active {
+  @apply bg-white text-[#2b1055];
 }
 </style>
