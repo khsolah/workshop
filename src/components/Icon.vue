5e1d29835e5214d7d71e33eaa621261100c408e5
khsolah <khsolah@gmail.com>
2021-07-14 22:11:42 +0800

    feat(component): icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <i class="block">
+    <svg class="w-full h-full" :fill="fill">
+      <use :href="`${require('../assets/icon/gen/index.svg')}#${name}`" />
+    </svg>
+  </i>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Icon',
+  props: {
+    fill: String,
+    name: String
+  }
+})
+</script>
