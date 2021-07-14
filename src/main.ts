db711640aaed16b7f077263fcdbd0e359983c691
khsolah <khsolah@gmail.com>
2021-07-14 22:09:09 +0800

    feat: init firebase

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Firebase from 'firebase/app'
 import router from './router'
 import store from './store'
 import 'windi.css'
+
+// Initialize Firebase
+Firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+})
 
 createApp(App).use(store).use(router).mount('#app')
