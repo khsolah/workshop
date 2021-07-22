3d8a7b241ae2c680531b36e94ec574bca3fda89a
khsolah <khsolah@gmail.com>
2021-07-22 21:35:15 +0800

    feat: store currentChannel

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,8 +1,10 @@
 export interface State {
+  currentChannel: firebase.default.database.DataSnapshot | null
   user: firebase.default.UserInfo | null
 }
 
 const state = (): State => ({
+  currentChannel: null,
   user: null
 })
 
