821c31b10b8ffc7aaabdf0c8500f0cf47fa4cc22
khsolah <khsolah@gmail.com>
2021-07-22 21:35:43 +0800

    feat: toggle currentChannel

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -2,7 +2,8 @@ import { MutationTree } from 'vuex'
 import { State } from './state'
 
 export enum MutationTypes {
-  SET_USER = 'SET_USER'
+  SET_USER = 'SET_USER',
+  SET_CURRENT_CHANNEL = 'SET_CURRENT_CHANNEL'
 }
 
 export interface Mutations<S = State> {
@@ -10,11 +11,18 @@ export interface Mutations<S = State> {
     state: S,
     payload: firebase.default.User | null
   ) => void
+  [MutationTypes.SET_CURRENT_CHANNEL]: (
+    state: S,
+    payload: firebase.default.database.DataSnapshot
+  ) => void
 }
 
 const mutations: MutationTree<State> & Mutations = {
   [MutationTypes.SET_USER]: (state, payload) => {
     state.user = payload
+  },
+  [MutationTypes.SET_CURRENT_CHANNEL]: (state, payload) => {
+    state.currentChannel = payload
   }
 }
 
