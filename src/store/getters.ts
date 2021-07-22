630540d47d37f81bbd0d14346ea3a6c0fe7d8831
khsolah <khsolah@gmail.com>
2021-07-22 21:34:50 +0800

    feat(store): get state: currentChannel

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -2,15 +2,18 @@ import { GetterTree } from 'vuex'
 import { State } from './state'
 
 export enum GetterTypes {
-  GET_USER = 'GET_USER'
+  GET_USER = 'GET_USER',
+  GET_CURRENT_CHANNEL = 'GET_CURRENT_CHANNEL'
 }
 
 export interface Getters<S = State> {
   [GetterTypes.GET_USER]: (state: S) => State['user']
+  [GetterTypes.GET_CURRENT_CHANNEL]: (state: S) => State['currentChannel']
 }
 
 const getters: GetterTree<State, State> & Getters = {
-  [GetterTypes.GET_USER]: ({ user }) => user
+  [GetterTypes.GET_USER]: ({ user }) => user,
+  [GetterTypes.GET_CURRENT_CHANNEL]: ({ currentChannel }) => currentChannel
 }
 
 export default getters
