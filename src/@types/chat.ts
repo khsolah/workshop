d3e503f4c62f2bd386dab2c286b1e9a3790909eb
khsolah <khsolah@gmail.com>
2021-07-25 05:32:09 +0800

    feat: vue-chat types

---
+++ src/@types/chat.ts
@@ -0,0 +1,22 @@
+export interface Channel {
+  id: string
+  name: string
+}
+
+export interface Message {
+  content: string
+  /* eslint @typescript-eslint/ban-types: 0 */
+  timestamp: Object
+  user: {
+    avatar: string | null
+    id: string
+    name: string | null
+  }
+}
+
+export interface User {
+  uid: string
+  name: string
+  avatar: string
+  status: 'online' | 'offline'
+}
